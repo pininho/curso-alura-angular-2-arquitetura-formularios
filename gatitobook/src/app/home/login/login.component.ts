b98cdea815d1b947b64c8cfba590d356fd01ef8f
Alessandro Muras de Oliveira Pino <alessandropino@caern.local>
2022-03-15 11:04:22 -0300

    implementacao metodo login

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AutentitacaoService } from 'src/app/autenticacao/autentitacao.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   usuario = '';
   senha = '';
 
-  constructor() { }
+  constructor(private authService: AutentitacaoService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-    console.log(this.usuario);
-    console.log(this.senha);
-  }
+    //console.log(this.usuario);
+    //console.log(this.senha);
 
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Autenticado com sucesso');
+      },
+      (error) => {
+        alert('Usuário ou senha inválido');
+        console.log(error);
+      }
+    );
+  }
 }
